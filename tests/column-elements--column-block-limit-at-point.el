acda84f37aea9424f53f8f6017d7c6c80c0674e1
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-10-04 11:58:00 -0700

    Pending test for (column-elements--column-block-limit-at-point)

--- tests/column-elements--column-block-limit-at-point.el
+++ tests/column-elements--column-block-limit-at-point.el
@@ -1,1 +1,3 @@
+(describe "Tests (column-elements--column-block-limit-at-point)"
+  (xit "Correctly finds the limits of column block 1 in example 1"))
 (provide 'column-elements--column-block-limit-at-point)
