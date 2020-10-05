36ed5a3e2b4ae471d6795d3fd82616f87b2635b2
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-10-05 06:53:22 -0700

    Fleshed out test for finding left limit of block 1, ex 1, point 0

--- tests/column-elements--column-block-limit-at-point.el
+++ tests/column-elements--column-block-limit-at-point.el
@@ -1,3 +1,12 @@
 (describe "Tests (column-elements--column-block-limit-at-point)"
-  (xit "Correctly finds the limits of column block 1 in example 1"))
+  :var (example-buffer-1)
+  (before-each
+    (setq example-buffer-1
+          (generate-new-buffer (generate-new-buffer-name "example-1"))))
+  (it "Finds the left limit of column block 1 in example 1 with point at 0"
+    (expect
+     (with-current-buffer example-buffer-1
+       (goto-char 0)
+       (column-elements--column-block-limit-at-point 'left)))
+    0))
 (provide 'column-elements--column-block-limit-at-point)
