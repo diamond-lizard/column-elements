e13d530ad3db7b11a3f77810957893f48226fedf
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-10-05 22:14:49 -0700

    Created (column-elements--delimiter-column-p) and aux

--- column-elements.el
+++ column-elements.el
@@ -12,6 +12,38 @@
 ;; What to use as a delimiter to determine column block boundaries.
 (setq column-elements--delimiter " ")
 
+(defun column-elements--delimiter-column-p-aux (column)
+  "Returns t if `COLUMN' contains only delimiters,
+otherwise returns nil."
+  (save-excursion
+    (save-restriction
+      (goto-char (point-min))
+      (not
+       (condition-case nil
+           (re-search-forward
+            (rx-to-string
+             `(seq
+               line-start
+               (= ,column anychar)
+               (not
+                (any ,column-elements--delimiter)))))
+         (search-failed nil))))))
+
+(defun column-elements--delimiter-column-p (&optional column)
+  "Returns t if the column at point contains only delimiters,
+otherwise returns nil."
+  (interactive)
+  (let* ((column
+          (if (boundp column)
+              column
+            (current-column)))
+         (current-column-is-a-delimiter-column
+          (column-elements--delimiter-column-p-aux (current-column))))
+    (progn
+      (message
+       (format "%s" current-column-is-a-delimiter-column))
+      current-column-is-a-delimiter-column)))
+
 ;;
 ;;------------------------------------------------------------------------
 
