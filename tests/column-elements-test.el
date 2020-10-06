ef99a1bfd7127ab2061cef9597691d513bdd16a9
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-10-05 23:08:03 -0700

    Now testing column-elements--delimiter-column-p-aux

--- tests/column-elements-test.el
+++ tests/column-elements-test.el
@@ -5,4 +5,5 @@
 (add-to-list 'load-path "tests")
 
 ;; The actual tests are split out in to the following files:
+(require 'column-elements--delimiter-column-p-aux)
 (require 'column-elements--column-block-limit-at-point)
