0bec185665fabd8008ee7c047a96d006269605f8
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-10-08 19:26:23 -0700

    Added license terms to each source file

--- column-elements.el
+++ column-elements.el
@@ -8,6 +8,23 @@
 ;; in gaps as needed.
 ;;
 ;; See README.org for details.
+;;
+;;
+;; Copyright (C) 2020 - Sergey Goldgaber
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+;;
+;; You should have received a copy of the GNU Affero General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 ;; What to use as a delimiter to determine column block boundaries.
 (defvar column-elements--delimiter " ")
