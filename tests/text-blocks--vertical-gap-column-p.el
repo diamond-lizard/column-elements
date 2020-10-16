b4548b5fc18035055f04e6ce50501b84f007bab7
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-10-15 18:24:46 -0700

    Rewrote tests for text-blocks--vertical-gap-column-p

--- tests/text-blocks--vertical-gap-column-p.el
+++ tests/text-blocks--vertical-gap-column-p.el
@@ -15,9 +15,56 @@
 ;; You should have received a copy of the GNU Affero General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(require 'text-blocks)
+(add-to-list 'load-path "tests")
 
-(ert-deftest text-blocks--vertical-gap-column-p--001 ()
+(require 'text-blocks)
+(require 'text-blocks--test-common)
+
+(setq text-blocks--filename-001 "tests/data/text-blocks-test-001")
+(setq text-blocks--filename-002 "tests/data/text-blocks-test-002")
+(setq text-blocks--filename-003 "tests/data/text-blocks-test-003")
+(setq text-blocks--filename-004 "tests/data/text-blocks-test-004")
+(setq text-blocks--filename-005 "tests/data/text-blocks-test-005")
+(setq text-blocks--filename-006 "tests/data/text-blocks-test-006")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Some global settings that these tests assume
+
+;; What to use as a delimiter to determine block boundaries.
+(setq text-blocks--block-delimiter " ")
+
+;; What to use as a delimiter to determine block row boundaries.
+(setq text-blocks--block-row-delimiter " ")
+
+;; A horizontal gap must have at least this many lines
+(setq text-blocks--min-lines-per-horiz-gap 1)
+
+;; A vertical gap must have at least this many columns
+(setq text-blocks--min-cols-per-vert-gap 2)
+
+;; End of global settings
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq text-blocks--test-metadata
+      '((test-id 03 data-file-id 001 position 001 column 000 line nil expect nil)
+        (test-id 04 data-file-id 001 position 006 column 000 line nil expect nil)
+        (test-id 05 data-file-id 001 position 007 column 000 line nil expect 't)
+        (test-id 06 data-file-id 001 position 008 column 000 line nil expect 't)
+        (test-id 07 data-file-id 001 position 009 column 000 line nil expect nil)
+        (test-id 08 data-file-id 001 position 015 column 000 line nil expect nil)))
+
+(setq text-blocks--test-name-prefix
+      "text-blocks")
+(setq text-blocks--test-buffer-name-prefix "text-blocks--original-data-00")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Tests that need to be written manually
+;;
+
+(ert-deftest text-blocks--001-vertical-gap-column-p ()
   "Make sure that text-blocks--vertical-gap-column-p is bound"
   :tags '(
           bindings
@@ -25,7 +72,132 @@
   (should
    (fboundp 'text-blocks--vertical-gap-column-p)))
 
-(setq text-blocks--filename-001 "tests/data/text-blocks-test-001")
+(ert-deftest text-blocks--002-vertical-gap-column-p ()
+  "Empty buffer returns nil"
+  :tags '(
+          empty-buffer
+          )
+  (should-not
+   (with-temp-buffer
+     (text-blocks--vertical-gap-column-p 0))))
+
+;;
+;; End of tests that need to be written manually
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Automatic test generation functions
+;;
+
+(defun text-blocks--create-test-name
+    (text-blocks--test-metadata-element)
+  "Generate test names like foo-001, foo-002, etc.."
+  (let* ((test-id (plist-get text-blocks--test-metadata-element 'test-id))
+         (data-file-id (plist-get text-blocks--test-metadata-element 'data-file-id))
+         (position (plist-get text-blocks--test-metadata-element 'position))
+         (column (plist-get text-blocks--test-metadata-element 'column))
+         (line (plist-get text-blocks--test-metadata-element 'line))
+         (expect (plist-get text-blocks--test-metadata-element 'expect))
+         (expect
+          (cond
+           ((equal (cadr expect) nil) nil)
+           ((equal (cadr expect) t) t)
+           ((equal (cadr expect) 'error) "error")
+           (t (error
+               (format
+                (concat
+                 "text-blocks--create-test-name: "
+                 "Error: "
+                 "unexpected 'expect' value '%s'")
+                expect))))))
+    (intern
+     (format
+      "%s--%03d-file-%s-position-%03d-col-%03d-line-%s-expect-%s"
+      text-blocks--test-name-prefix
+      test-id
+      data-file-id
+      position
+      column
+      line
+      expect))))
+
+;; Automated generation of test bodies
+(defun text-blocks--get-test-body
+    (text-blocks--test-metadata-element)
+  (let* ((test-id (plist-get text-blocks--test-metadata-element 'test-id))
+         (data-file-id (plist-get text-blocks--test-metadata-element 'data-file-id))
+         (position (plist-get text-blocks--test-metadata-element 'position))
+         (column (plist-get text-blocks--test-metadata-element 'column))
+         (line (plist-get text-blocks--test-metadata-element 'line))
+         (expect (plist-get text-blocks--test-metadata-element 'expect))
+         (data-file-buffer-name
+          (get-data-file-buffer-name data-file-id)))
+    `(lambda ()
+       (let ((test-result
+              (with-temp-buffer
+                (replace-buffer-contents
+                 ,data-file-buffer-name)
+                (goto-char ,position)
+                (if (equal ,expect 'error)
+                    (should-error
+                     (text-blocks--vertical-gap-column-p
+                      ,column
+                      ,line))
+                  (text-blocks--vertical-gap-column-p
+                   ,column
+                   ,line)))))
+         (cond
+          ((equal test-result ,expect) (ert-pass))
+          ;; Expected errors should pass
+          ;;
+          ;; On error, ERT's should-error returns a cons pair
+          ;; containing 'error as the first element,
+          ;; so if we are expecting an error, we check for that
+          ((and
+            (equal
+             ,expect
+             'error)
+            (equal
+             (type-of test-result)
+             'cons)
+            (equal
+             (car test-result)
+             'error))
+           (ert-pass))
+          ((equal test-result nil)
+           (ert-fail
+            (print
+             (format
+              (concat
+               "Test: '%s' File '%s' Position '%s' Column '%s' Line '%s'\n"
+               "Expected '%s' but got '%s'")
+              ,test-id
+              ,data-file-id
+              ,position
+              ,column
+              ,line
+              ,expect
+              test-result))))
+          ;; Everything else fails
+          ('otherwise
+           (ert-fail
+            (print
+             (format
+              "Unexpected test result '%s'.  This should always be t or nil."
+              test-result)))))))))
+
+;;
+;; End of automatic test generation functions
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Automatically generated tests
+;;
+
 
 ;; Read in test file 001, if it exists.
 (if (file-exists-p text-blocks--filename-001)
@@ -33,76 +205,25 @@
           (find-file-read-only text-blocks--filename-001))
   (error "File '%s' does not exist" text-blocks--filename-001))
 
-(ert-deftest text-blocks--vertical-gap-column-p--002 ()
-  "column 0 in data/001 is not a vertical gap"
-  :tags '(
-          not-vertical-gap
-          )
-  (should-not
-   (with-temp-buffer
-     (replace-buffer-contents text-blocks--original-data-001)
-       (text-blocks--vertical-gap-column-p 0))))
+(cl-loop
+ for text-blocks--test-metadata-element in text-blocks--test-metadata
+ do (let* ((name (text-blocks--create-test-name
+                  text-blocks--test-metadata-element))
+           (expect (plist-get text-blocks--test-metadata-element 'expect)))
+      (ert-set-test
+       name
+       (make-ert-test
+        :expected-result-type (if (equal expect 'error)
+                                  :error
+                                :passed)
+        :name name
+        :body (text-blocks--get-test-body
+               text-blocks--test-metadata-element)))))
 
-(ert-deftest text-blocks--vertical-gap-column-p--003 ()
-  "column 1 in data/001 is not a vertical gap"
-  :tags '(
-          not-vertical-gap
-          )
-  (should-not
-   (with-temp-buffer
-     (replace-buffer-contents text-blocks--original-data-001)
-       (text-blocks--vertical-gap-column-p 1))))
+;;
+;; End of automatically generated tests
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(ert-deftest text-blocks--vertical-gap-column-p--004 ()
-  "column 3 in data/001 is not a vertical gap"
-  :tags '(
-          not-vertical-gap
-          )
-  (should-not
-   (with-temp-buffer
-     (replace-buffer-contents text-blocks--original-data-001)
-       (text-blocks--vertical-gap-column-p 3))))
-
-(ert-deftest text-blocks--vertical-gap-column-p--005 ()
-  "column 6 in data/001 is a vertical gap"
-  :tags '(
-          is-vertical-gap
-          )
-  (should
-   (with-temp-buffer
-     (replace-buffer-contents text-blocks--original-data-001)
-       (text-blocks--vertical-gap-column-p 6))))
-
-(ert-deftest text-blocks--vertical-gap-column-p--006 ()
-  "column 8 in data/001 is not a vertical gap"
-  :tags '(
-          not-vertical-gap
-          )
-  (should-not
-   (with-temp-buffer
-     (replace-buffer-contents text-blocks--original-data-001)
-       (text-blocks--vertical-gap-column-p 8))))
-
-(ert-deftest text-blocks--vertical-gap-column-p--007 ()
-  "column 12 in data/001 is not a vertical gap"
-  :tags '(
-          not-vertical-gap
-          )
-  (should-not
-   (with-temp-buffer
-     (replace-buffer-contents text-blocks--original-data-001)
-       (text-blocks--vertical-gap-column-p 12))))
-
-(ert-deftest text-blocks--vertical-gap-column-p--008 ()
-  "checking to see if column -1 in data/001 is a vertical gap errors out"
-  :tags '(
-          error
-          out-of-bounds
-          invalid-argument
-          )
-  (should-error
-   (with-temp-buffer
-     (replace-buffer-contents text-blocks--original-data-001)
-       (text-blocks--vertical-gap-column-p -1))))
 
 (provide 'text-blocks--vertical-gap-column-p)
