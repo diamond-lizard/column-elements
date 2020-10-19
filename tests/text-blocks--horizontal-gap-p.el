766ca5e7c86b7c7d1a7472647ed0d9809b5df616
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-10-19 13:32:52 -0700

    Replaced cond with pcase

--- tests/text-blocks--horizontal-gap-p.el
+++ tests/text-blocks--horizontal-gap-p.el
@@ -163,8 +163,8 @@
                      (print
                       (format
                        "Expected a line or a position, but found neither")))))))))
-         (cond
-          ((equal horizontal-gap-test-result t)
+         (pcase horizontal-gap-test-result
+          ('t
            (cond
             ((equal ,expect 'horizontal-gap) (ert-pass))
             ((equal ,expect 'not-horizontal-gap)
@@ -179,7 +179,7 @@
                     ,position
                   ,line)
                 ,data-file-id))))))
-          ((equal horizontal-gap-test-result nil)
+          ('nil
            (cond
             ((equal ,expect 'not-horizontal-gap) (ert-pass))
             ((equal ,expect 'horizontal-gap)
@@ -200,13 +200,8 @@
           ;; containing 'error as the first element,
           ;; so if we are expecting an error, we check for that
           ((and
-            (equal
-             ,expect
-             'error)
-            (consp horizontal-gap-test-result)
-            (equal
-             (car horizontal-gap-test-result)
-             'error))
+            (guard (equal ,expect 'error))
+            `(error ,_))
            (ert-pass))
           ;; Everything else fails
           ('otherwise
