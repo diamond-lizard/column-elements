9de251f4ed9f2a5d7ccb14380a796e6425875699
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-10-19 14:51:58 -0700

    Replaced cond with pcase

--- tests/text-blocks--vertical-gap-p-04.el
+++ tests/text-blocks--vertical-gap-p-04.el
@@ -186,8 +186,8 @@
                  ,data-file-buffer-name)
                 (goto-char ,position)
                 (text-blocks--vertical-gap-p))))
-         (cond
-          ((equal t result)
+         (pcase result
+          ('t
            (cond
             ((equal ,expect 'vertical-gap) (ert-pass))
             ((equal ,expect 'not-vertical-gap)
@@ -196,7 +196,7 @@
                (format
                 "At position '%s' in file '%s' expected non-gap but got gap"
                 ,position ,data-file-id))))))
-          ((equal nil result)
+          ('nil
            (cond
             ((equal ,expect 'not-vertical-gap) (ert-pass))
             ((equal ,expect 'vertical-gap)
@@ -205,7 +205,7 @@
                (format
                 "At position '%s' in file '%s' expected gap but got not-gap"
                 ,position ,data-file-id))))))
-          ('otherwise
+          (_
            (ert-fail
             (print
              (format
